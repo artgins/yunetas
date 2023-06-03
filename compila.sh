428e2b07d75a40da634ea077fc1947effca3c0c8
niyamaka <ginsmar@gmail.com>
2023-06-03 19:06:37 +0200

    reorganizando

---
+++ compila.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#  Exit immediately if a command exits with a non-zero status.
+set -e
+
+##########################################
+#       gobj
+##########################################
+cd /yuneta/development/yuneta/yunetas/gobj
+rm -rf build; mkdir build
+cd build; cmake -DCMAKE_BUILD_TYPE=Debug ..
+make install
+
+##########################################
+#       core-linux
+##########################################
+cd /yuneta/development/yuneta/yunetas/core-linux
+rm -rf build; mkdir build
+cd build; cmake -DCMAKE_BUILD_TYPE=Debug ..
+make install
+
+##########################################
+#       c_prot
+##########################################
+cd /yuneta/development/yuneta/yunetas/c_prot
+rm -rf build; mkdir build
+cd build; cmake -DCMAKE_BUILD_TYPE=Debug ..
+make install
+
+##########################################
+#       all
+##########################################
+cd /yuneta/development/yuneta/yunetas
+rm -rf build; mkdir build
+cd build; cmake -DCMAKE_BUILD_TYPE=Debug ..
+make install
