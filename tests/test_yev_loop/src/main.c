61de0100da40a3009d110bb7fa18cb9126ab4211
niyamaka <ginsmar@gmail.com>
2023-06-21 08:10:44 +0200

    add tests

---
+++ tests/test_yev_loop/src/main.c
@@ -0,0 +1,126 @@
+/***************************************************************************
+ *                      Main
+ ***************************************************************************/
+#include <string.h>
+#include <gobj.h>
+#include <c_linux_yuno.h>
+#include <signal.h>
+#include <log_udp_handler.h>
+#include <gobj_environment.h>
+#include <stacktrace_with_bfd.h>
+
+
+PUBLIC void yuno_catch_signals(void);
+
+int main(int argc, char *argv[])
+{
+    /*----------------------------------*
+     *      Startup gobj system
+     *----------------------------------*/
+    sys_malloc_fn_t malloc_func;
+    sys_realloc_fn_t realloc_func;
+    sys_calloc_fn_t calloc_func;
+    sys_free_fn_t free_func;
+
+    gobj_get_allocators(
+        &malloc_func,
+        &realloc_func,
+        &calloc_func,
+        &free_func
+    );
+
+    json_set_alloc_funcs(
+        malloc_func,
+        free_func
+    );
+
+    gobj_set_deep_tracing(2);           // TODO TEST
+    gobj_set_global_trace(0, TRUE);     // TODO TEST
+
+#ifdef DEBUG
+    init_backtrace_with_bfd(argv[0]);
+    set_show_backtrace_fn(show_backtrace_with_bfd);
+#endif
+
+    gobj_start_up(
+        argc,
+        argv,
+        NULL, // jn_global_settings
+        NULL, // startup_persistent_attrs
+        NULL, // end_persistent_attrs
+        0,  // load_persistent_attrs
+        0,  // save_persistent_attrs
+        0,  // remove_persistent_attrs
+        0,  // list_persistent_attrs
+        NULL, // global_command_parser
+        NULL, // global_stats_parser
+        NULL, // global_authz_checker
+        NULL, // global_authenticate_parser
+        8*1024L,    // max_block, largest memory block
+        100*1024L   // max_system_memory, maximum system memory
+    );
+
+    yuno_catch_signals();
+
+    /*--------------------------------*
+     *      Log handlers
+     *--------------------------------*/
+    gobj_log_add_handler("stdout", "stdout", LOG_OPT_ALL, 0);
+
+
+    return gobj_get_exit_code();
+}
+
+/***************************************************************************
+ *      Signal handlers
+ ***************************************************************************/
+PRIVATE void quit_sighandler(int sig)
+{
+    static int tries = 0;
+
+    /*
+     *  __yuno_gobj__ is 0 for watcher fork, if we are running --stop
+     */
+    hgobj gobj = gobj_yuno();
+
+    if(gobj) {
+        tries++;
+        gobj_set_yuno_must_die();
+        if(tries > 1) {
+            _exit(-1);
+        }
+    }
+}
+
+PRIVATE void debug_sighandler(int sig)
+{
+    /*
+     *  __yuno_gobj__ is 0 for watcher fork, if we are running --stop
+     */
+    hgobj gobj = gobj_yuno();
+
+    if(gobj) {
+        gobj_set_deep_tracing(2);
+    }
+}
+
+PUBLIC void yuno_catch_signals(void)
+{
+    struct sigaction sigIntHandler;
+
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGTERM, SIG_IGN);
+
+    memset(&sigIntHandler, 0, sizeof(sigIntHandler));
+    sigIntHandler.sa_handler = quit_sighandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = SA_NODEFER|SA_RESTART;
+    sigaction(SIGALRM, &sigIntHandler, NULL);   // to debug in kdevelop
+    sigaction(SIGQUIT, &sigIntHandler, NULL);
+    sigaction(SIGINT, &sigIntHandler, NULL);    // ctrl+c
+
+    sigIntHandler.sa_handler = debug_sighandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = SA_NODEFER|SA_RESTART;
+    sigaction(SIGUSR1, &sigIntHandler, NULL);
+}
