892c0055603ae0589f6f279a6f39fc150c24fef3
niyamaka <ginsmar@gmail.com>
2023-08-04 08:56:10 +0200

    comm_prot

--- gobj/src/comm_prot.c
+++ gobj/src/comm_prot.c
@@ -16,6 +16,12 @@
 /***************************************************************
  *              Structures
  ***************************************************************/
+typedef struct {
+    DL_ITEM_FIELDS
+
+    gclass_name_t gclass_name;
+    const char *schema;
+} comm_prot_t;
 
 /***************************************************************
  *              Prototypes
@@ -25,7 +31,7 @@
  *              Data
  ***************************************************************/
 PRIVATE char __initialized__ = FALSE;
-PRIVATE dl_list_t dl_prot;
+PRIVATE dl_list_t dl_comm_prot;
 
 /***************************************************************************
  *  Register a gclass with a communication protocol
@@ -34,10 +40,26 @@ PUBLIC int comm_prot_register(gclass_name_t gclass_name, const char *schema)
 {
     if(!__initialized__) {
         __initialized__ = TRUE;
-        dl_init(&dl_prot);
+        dl_init(&dl_comm_prot);
     }
-// TODO
-    return 0;
+
+    comm_prot_t *lh = GBMEM_MALLOC(sizeof(comm_prot_t));
+    if(!lh) {
+        gobj_log_error(0, 0,
+            "function",     "%s", __FUNCTION__,
+            "msgset",       "%s", MSGSET_INTERNAL_ERROR,
+            "msg",          "%s", "No memory to comm_prot_t",
+            NULL
+        );
+        return -1;
+    }
+    lh->schema = GBMEM_STRDUP(schema);
+    lh->gclass_name = gclass_name;
+
+    /*----------------*
+     *  Add to list
+     *----------------*/
+    return dl_add(&dl_comm_prot, lh);
 }
 
 /***************************************************************************
@@ -45,9 +67,26 @@ PUBLIC int comm_prot_register(gclass_name_t gclass_name, const char *schema)
  ***************************************************************************/
 PUBLIC gclass_name_t comm_prot_get_gclass(const char *schema)
 {
-    gclass_name_t gclass_name;
-// TODO
-    return gclass_name;
+    comm_prot_t *lh = dl_first(&dl_comm_prot);
+    while(lh) {
+        comm_prot_t *next = dl_next(lh);
+        if(strcmp(lh->schema, schema)==0) {
+            return lh->gclass_name;
+        }
+        /*
+         *  Next
+         */
+        lh = next;
+    }
+
+    gobj_log_error(0, LOG_OPT_TRACE_STACK,
+        "function",     "%s", __FUNCTION__,
+        "msgset",       "%s", MSGSET_INTERNAL_ERROR,
+        "msg",          "%s", "gclass for schema not found",
+        "schema",       "%s", schema,
+        NULL
+    );
+    return NULL;
 }
 
 /***************************************************************************
@@ -55,5 +94,12 @@ PUBLIC gclass_name_t comm_prot_get_gclass(const char *schema)
  ***************************************************************************/
 PUBLIC void comm_prot_free(void)
 {
-// TODO
+    comm_prot_t *lh;
+    while((lh=dl_first(&dl_comm_prot))) {
+        dl_delete(&dl_comm_prot, lh, 0);
+        GBMEM_FREE(lh->schema);
+        GBMEM_FREE(lh);
+    }
+
+    __initialized__ = FALSE;
 }
