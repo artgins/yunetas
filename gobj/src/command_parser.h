5d4e74c0d39f6310c5db4453c55d3fc10a5ee476
niyamaka <ginsmar@gmail.com>
2023-08-09 06:37:16 +0200

    add command and stats parser

---
+++ gobj/src/command_parser.h
@@ -0,0 +1,56 @@
+/****************************************************************************
+ *          COMMAND_PARSER.H
+ *
+ *          Command parser
+ *
+ *          Copyright (c) 2017 Niyamaka.
+ *          All Rights Reserved.
+ ****************************************************************************/
+#pragma once
+
+#include "gobj.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/***************************************************
+ *              Prototypes
+ **************************************************/
+PUBLIC json_t * command_parser(
+    hgobj gobj,
+    const char *command,
+    json_t *kw,
+    hgobj src
+);
+
+PUBLIC json_t *gobj_build_cmds_doc(
+    hgobj gobj,
+    json_t *kw
+);
+
+PUBLIC const sdata_desc_t *command_get_cmd_desc(
+    const sdata_desc_t *command_table,
+    const char *cmd
+);
+
+PUBLIC json_t *expand_command(
+    const char *gobj_name,
+    const sdata_desc_t *command_table,
+    const char *command,
+    json_t *kw,     // NOT owned
+    const sdata_desc_t **cmd_desc
+);
+
+PUBLIC json_t *build_cmd_kw(
+    const char *gobj_name,
+    const char *command,
+    const sdata_desc_t *cnf_cmd,
+    char *parameters,
+    json_t *kw, // not owned
+    int *result
+);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ gobj/src/stats_parser.h
@@ -0,0 +1,38 @@
+/****************************************************************************
+ *          STATS_PARSER.H
+ *
+ *          Stats parser
+ *
+ *          Copyright (c) 2017 Niyamaka.
+ *          All Rights Reserved.
+ ****************************************************************************/
+#pragma once
+
+#include "gobj.h"
+#include "msg_ievent.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/***************************************************
+ *              Prototypes
+ **************************************************/
+PUBLIC json_t * stats_parser(
+    hgobj gobj,
+    const char *stats,
+    json_t *kw,
+    hgobj src
+);
+
+PUBLIC json_t *build_stats( // Build stats from gobj's attributes with SFD_STATS flag.
+    hgobj gobj,
+    const char *stats,
+    json_t *kw,
+    hgobj src
+);
+
+
+#ifdef __cplusplus
+}
+#endif
