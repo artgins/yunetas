a35561b5c6e78b3ccaf910cc222acb1c587ddad5
rmm <rosa.martinez@artgins.com>
2024-02-25 11:07:43 +0100

    cmake, make a function to convert .config in .h

--- tools/cmake/project.cmake
+++ tools/cmake/project.cmake
@@ -19,6 +19,7 @@ set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} /yuneta/development/outputs/lib)
 
 if(CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-DDEBUG)
+# TODO check if -fno-stack-protector is only for esp32
     add_compile_options(-std=c99 -Wall -Wextra -g3 -fno-pie -fno-stack-protector -Wno-unused-parameter -fPIC)
     add_link_options(-no-pie)
 else()
@@ -35,3 +36,27 @@ endif()
 #MESSAGE(STATUS "DIR ${CMAKE_CURRENT_SOURCE_DIR}")
 #MESSAGE(STATUS "COMPILER_ID ${CMAKE_C_COMPILER_ID}")
 #MESSAGE(STATUS "COMPILER ${CMAKE_C_COMPILER}")
+
+function(kconfig2include CONFIG_H_CONTENT CONFIG_LINES)
+    foreach(line ${CONFIG_LINES})
+        if(line MATCHES "^#.*" OR line MATCHES "^$")
+            # Skip comments and empty lines
+        else()
+            # Extract configuration key and value
+            string(REPLACE "=" ";" CONFIG_PAIR ${line})
+            list(GET CONFIG_PAIR 0 CONFIG_KEY)
+            list(GET CONFIG_PAIR 1 CONFIG_VALUE)
+            # Remove possible quotes from string values
+            string(REPLACE "\"" "" CONFIG_VALUE ${CONFIG_VALUE})
+
+            # Append the configuration to the header content
+            if("${CONFIG_VALUE}" STREQUAL "y")
+                set(CONFIG_H_CONTENT "${CONFIG_H_CONTENT}#define ${CONFIG_KEY} 1\n" PARENT_SCOPE)
+            elseif("${CONFIG_VALUE}" MATCHES "^[0-9]+$")
+                set(CONFIG_H_CONTENT "${CONFIG_H_CONTENT}#define ${CONFIG_KEY} ${CONFIG_VALUE}\n" PARENT_SCOPE)
+            else()
+                set(CONFIG_H_CONTENT "${CONFIG_H_CONTENT}#define ${CONFIG_KEY} \"${CONFIG_VALUE}\"\n" PARENT_SCOPE)
+            endif()
+        endif()
+    endforeach()
+endfunction()
