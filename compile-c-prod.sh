84ef93f7a2b9ad9e7902a68534d040db2d71cf2d
rmm <rosa.martinez@artgins.com>
2024-08-16 10:24:47 +0200

    fix compile scripts

--- compile-c-prod.sh
+++ compile-c-prod.sh
@@ -30,7 +30,7 @@ make install
 ##########################################
 #       c_prot
 ##########################################
-cd /yuneta/development/yunetas/libs/c/c_prot
+cd /yuneta/development/yunetas/modules/c/c_prot
 rm -rf build; mkdir build
 cd build; cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 make install
