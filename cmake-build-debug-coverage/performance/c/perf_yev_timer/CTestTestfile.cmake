a4a83cdbb36e24356f454b23002e053cf523f485
rmm <rosa.martinez@artgins.com>
2024-08-20 11:28:29 +0200

    remove cmake-build-debug-coverage

--- cmake-build-debug-coverage/performance/c/perf_yev_timer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /yuneta/development/yunetas/performance/c/perf_yev_timer
-# Build directory: /yuneta/development/yunetas/cmake-build-debug-coverage/performance/c/perf_yev_timer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug-coverage/tests/c/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /yuneta/development/yunetas/tests/c
-# Build directory: /yuneta/development/yunetas/cmake-build-debug-coverage/tests/c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gobj")
-subdirs("timeranger2")
