d641796dfe71e9e567df9e04d3b38cb58775e14f
rmm <rosa.martinez@artgins.com>
2024-09-19 19:18:41 +0200

    fs_watcher

---
+++ kernel/c/root-linux/src/fs_watcher.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+ *              fs_watcher.h
+ *
+ *              Monitoring of directories and files with io_uring
+ *
+ *              Copyright (c) 2024 ArtGins.
+ *              All Rights Reserved.
+ ****************************************************************************/
+#pragma once
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "yunetas_ev_loop.h"
+
+/*********************************************************************
+ *  Prototypes
+ *********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
