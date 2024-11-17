7b9b9ee6e36f52948c503e7e9ecaaea264cebc69
rmm <rosa.martinez@artgins.com>
2024-11-17 12:33:41 +0100

    test_topic_pkey_integer_iterator6

--- tests/c/timeranger2/test_topic_pkey_integer_iterator6.c
+++ tests/c/timeranger2/test_topic_pkey_integer_iterator6.c
@@ -14,10 +14,14 @@
 #include <timeranger2.h>
 #include <stacktrace_with_bfd.h>
 #include <yev_loop.h>
+#include <launch_daemon.h>
 #include <kwid.h>
 #include <testing.h>
 
-#define APP "test_topic_pkey_integer_iterator6"
+#define APP     "test_topic_pkey_integer_iterator6"
+#define DOC     "test_topic_pkey_integer_iterator6"
+#define SUPPORT "<niyamaka at yuneta.io>"
+#define VERSION "1.0"
 
 #define DATABASE    "tr_topic_pkey_integer"
 #define TOPIC_NAME  "topic_pkey_integer"
@@ -26,6 +30,83 @@
 
 PRIVATE int pinta_md = 1;
 PRIVATE int pinta_records = 0;
+
+/***************************************************************************
+ *              Arguments
+ ***************************************************************************/
+#include <argp.h>
+
+#define MIN_ARGS 0
+#define MAX_ARGS 0
+struct arguments {
+    char *args[MAX_ARGS+1];     /* positional args */
+
+    int client;
+};
+
+const char *argp_program_version = APP " " VERSION;
+const char *argp_program_bug_address = SUPPORT;
+
+/* Program documentation. */
+static char doc[] = DOC;
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "";
+
+/*
+ *  The options we understand.
+ *  See https://www.gnu.org/software/libc/manual/html_node/Argp-Option-Vectors.html
+ */
+static struct argp_option options[] = {
+/*-name---------key-----arg-----flags---doc---------------------------------group */
+{"client",      'c',    0,      0,      "Run as client, default is master", 0},
+{0}
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+    /*
+     *  Get the input argument from argp_parse,
+     *  which we know is a pointer to our arguments structure.
+     */
+    struct arguments *arguments_ = state->input;
+
+    switch (key) {
+    case 'c':
+        arguments_->client = 1;
+        break;
+    case ARGP_KEY_ARG:
+        if (state->arg_num >= MAX_ARGS) {
+            /* Too many arguments_. */
+            argp_usage (state);
+        }
+        arguments_->args[state->arg_num] = arg;
+        break;
+
+    case ARGP_KEY_END:
+        if (state->arg_num < MIN_ARGS) {
+            /* Not enough arguments_. */
+            argp_usage (state);
+        }
+        break;
+
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = {
+    options,
+    parse_opt,
+    args_doc,
+    doc,
+    0,
+    0,
+    0
+};
+struct arguments arguments;
 
 /***************************************************************
  *              Prototypes
@@ -98,6 +179,17 @@ PRIVATE int rt_disk_record_callback(
 }
 
 /***************************************************************************
+ *
+ ***************************************************************************/
+int yev_callback(yev_event_t *event) {
+    if(!arguments.client) {
+        // In master break on timeout
+        return -1;
+    }
+    return 0;
+}
+
+/***************************************************************************
  *              Test
  *  Open as master, open iterator (realtime by disk) with callback
  *  HACK: return -1 to fail, 0 to ok
@@ -132,13 +224,13 @@ PRIVATE int do_test(json_t *tranger)
             "key", KEY,
             "load_record_callback", (json_int_t)(size_t)rt_disk_record_callback
         );
-        tranger2_open_list(
+        json_t *list = tranger2_open_list(
             tranger,
             TOPIC_NAME,
             match_cond,             // match_cond, owned
             NULL,                   // extra
-            "it_by_disk",           // rt_id
-            FALSE,                  // rt_by_disk
+            arguments.client?"it_by_disk":NULL, // rt_id
+            arguments.client,       // rt_by_disk
             NULL                    // creator
         );
 
@@ -171,25 +263,29 @@ PRIVATE int do_test(json_t *tranger)
         }
         t1++;   // begin by the next
 
-        for(json_int_t i=0; i<MAX_KEYS; i++) {
-            uint64_t tm = t1;
-            for(json_int_t j=0; j<MAX_RECORDS; j++) {
-                json_t *jn_record1 = json_pack("{s:I, s:I, s:s}",
-                   "id", i + 1,
-                   "tm", tm+j,
-                   "content",
-                   "Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el."
-                   "Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el."
-                   "Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el.x"
-                );
-                md2_record_ex_t md_record;
-                tranger2_append_record(tranger, TOPIC_NAME, tm+j, 0, &md_record, jn_record1);
-                if(i % 10 == 0) {
-                    yev_loop_run_once(yev_loop);
+        if(!arguments.client) {
+            for(json_int_t i=0; i<MAX_KEYS; i++) {
+                uint64_t tm = t1;
+                for(json_int_t j=0; j<MAX_RECORDS; j++) {
+                    json_t *jn_record1 = json_pack("{s:I, s:I, s:s}",
+                       "id", i + 1,
+                       "tm", tm+j,
+                       "content",
+                       "Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el."
+                       "Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el."
+                       "Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el alfa.Pepe el.x"
+                    );
+                    md2_record_ex_t md_record;
+                    tranger2_append_record(tranger, TOPIC_NAME, tm+j, 0, &md_record, jn_record1);
+                    if(i % 10 == 0) {
+                        yev_loop_run_once(yev_loop);
+                    }
                 }
             }
+            yev_loop_run_once(yev_loop);
+        } else {
+            yev_loop_run(yev_loop, -1);
         }
-        yev_loop_run_once(yev_loop);
 
         MT_INCREMENT_COUNT(time_measure, MAX_KEYS*MAX_RECORDS)
         MT_PRINT_TIME(time_measure, "append records")
@@ -205,25 +301,20 @@ PRIVATE int do_test(json_t *tranger)
         }
 
         result += test_json(NULL);  // NULL: we want to check only the logs
-    }
 
-    /*-------------------------------------*
-     *      Close disk iterator
-     *-------------------------------------*/
-    set_expected_results( // Check that no logs happen
-        "close disk iterator", // test name
-        NULL,   // error's list, It must not be any log error
-        NULL,   // expected, NULL: we want to check only the logs
-        NULL,   // ignore_keys
-        TRUE    // verbose
-    );
-    if(tranger2_get_iterator_by_id(tranger, TOPIC_NAME, "it_by_disk", "")) {
-        result += tranger2_close_iterator(
-            tranger,
-            tranger2_get_iterator_by_id(tranger, TOPIC_NAME, "it_by_disk", "")
+        /*-------------------------------------*
+         *      Close list
+         *-------------------------------------*/
+        set_expected_results( // Check that no logs happen
+            "close list", // test name
+            NULL,   // error's list, It must not be any log error
+            NULL,   // expected, NULL: we want to check only the logs
+            NULL,   // ignore_keys
+            TRUE    // verbose
         );
+        tranger2_close_list(tranger, list);
+        result += test_json(NULL);  // NULL: we want to check only the logs
     }
-    result += test_json(NULL);  // NULL: we want to check only the logs
 
     /*-------------------------------*
      *  tranger_backup_topic
@@ -265,7 +356,7 @@ PRIVATE json_t *open_all(void)
     json_t *jn_tranger = json_pack("{s:s, s:s, s:b, s:i}",
         "path", path_root,
         "database", DATABASE,
-        "master", 1,
+        "master", !arguments.client,
         "on_critical_error", 0
     );
     json_t *tranger = tranger2_startup(0, jn_tranger, yev_loop);
@@ -330,6 +421,20 @@ PRIVATE int close_all(json_t *tranger)
  ***************************************************************************/
 int main(int argc, char *argv[])
 {
+    /*---------------------------------*
+     *      Arguments
+     *---------------------------------*/
+    memset(&arguments, 0, sizeof(arguments));
+    /*
+     *  Default values
+     */
+    arguments.client = 0;
+
+    /*
+     *  Parse arguments
+     */
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
     /*----------------------------------*
      *      Startup gobj system
      *----------------------------------*/
@@ -408,7 +513,7 @@ int main(int argc, char *argv[])
         0,
         2024,
         10,
-        NULL,
+        yev_callback,
         &yev_loop
     );
 
@@ -417,13 +522,33 @@ int main(int argc, char *argv[])
      *--------------------------------*/
     json_t *tranger = open_all();
 
+    int pid_test_client = -1;
+
+    if(!arguments.client) {
+        #define CLIENT "./tests/c/timeranger2/test_topic_pkey_integer_iterator6"
+        pid_test_client = launch_daemon(
+            FALSE,
+            CLIENT,
+            "-c",
+            NULL
+        );
+        if(pid_test_client < 0) {
+            printf("%sERROR%s --> Cannot launch client test: %s\n", On_Red BWhite, Color_Off, CLIENT);
+            global_result += -1;
+        }
+        sleep(1);
+    }
     global_result += do_test(tranger);
 
     yev_loop_run_once(yev_loop);
 
     int result = close_all(tranger);
 
-    yev_loop_run_once(yev_loop);
+    yev_loop_run(yev_loop, 1);
+
+    if(pid_test_client > 0) {
+        kill(pid_test_client, SIGQUIT);
+    }
 
     /*--------------------------------*
      *  Stop the event loop
