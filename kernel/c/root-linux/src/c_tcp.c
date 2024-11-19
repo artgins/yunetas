d1e99443ef4f109d7bdc497c32841b417d29ffa3
rmm <rosa.martinez@artgins.com>
2024-11-19 12:24:49 +0100

    clear

--- kernel/c/root-linux/src/c_tcp.c
+++ kernel/c/root-linux/src/c_tcp.c
@@ -767,8 +767,6 @@ int send_clear_data(hytls ytls, hsskt sskt, gbuffer_t *gbuf)
  ***************************************************************************/
 PRIVATE int ytls_on_handshake_done_callback(hgobj gobj, int error)
 {
-    PRIVATE_DATA *priv = gobj_priv_data(gobj);
-
     if(gobj_trace_level(gobj) & TRACE_CONNECT_DISCONNECT) {
         gobj_log_debug(gobj, 0,
             "msgset",       "%s", MSGSET_CONNECT_DISCONNECT,
