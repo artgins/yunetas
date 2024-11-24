b7f0cb9c1d9a535340b597b50b00c4a244ebe7ae
rmm <rosa.martinez@artgins.com>
2024-11-24 16:21:54 +0100

    make a function to get cache files and cache total

--- kernel/c/timeranger2/src/timeranger2.c
+++ kernel/c/timeranger2/src/timeranger2.c
@@ -232,6 +232,10 @@ PRIVATE int update_totals_of_key_cache(
 );
 
 PRIVATE json_int_t get_topic_key_rows(hgobj gobj, json_t *topic, const char *key);
+
+PRIVATE json_t *get_cache_files(json_t *topic, const char *key);
+PRIVATE json_t *get_cache_total(json_t *topic, const char *key);
+
 PRIVATE json_t *get_segments(
     hgobj gobj,
     json_t *tranger,
@@ -5451,11 +5455,6 @@ PUBLIC json_t *tranger2_iterator_get_page( // return must be owned
  ***************************************************************************/
 PRIVATE json_t *get_cache_files(json_t *topic, const char *key)
 {
-// Use `, don't use json_object_get() WHY?
-//    char path[NAME_MAX];
-//    snprintf(path, sizeof(path), "cache`%s`files", key);
-//    json_t *cache_files = kw_get_list(gobj, topic, path, 0, 0);
-
     json_t *cache = json_object_get(topic, "cache");
     if(!cache) {
         return NULL;
@@ -5473,10 +5472,6 @@ PRIVATE json_t *get_cache_files(json_t *topic, const char *key)
  ***************************************************************************/
 PRIVATE json_t *get_cache_total(json_t *topic, const char *key)
 {
-//    char path[NAME_MAX];
-//    snprintf(path, sizeof(path), "cache`%s`total", key);
-//    json_t *cache_total = kw_get_dict(gobj, topic, path, 0, 0);
-
     json_t *cache = json_object_get(topic, "cache");
     if(!cache) {
         return NULL;
@@ -5511,9 +5506,6 @@ PRIVATE json_t *get_segments(
     /*-------------------------------------*
      *      Recover cache data
      *-------------------------------------*/
-//    char path[NAME_MAX];
-//    snprintf(path, sizeof(path), "cache`%s`files", key);
-//    json_t *cache_files = kw_get_list(gobj, topic, path, 0, 0); // Use `, don't use json_object_get()
     json_t *cache_files = get_cache_files(topic, key);
     if(!cache_files) {
         gobj_log_error(gobj, 0,
@@ -5527,9 +5519,6 @@ PRIVATE json_t *get_segments(
         return jn_segments;
     }
 
-//    snprintf(path, sizeof(path), "cache`%s`total", key);
-//    json_t *cache_total = kw_get_dict(gobj, topic, path, 0, 0);
-//    if(!cache_total) {
     json_t *cache_total = get_cache_total(topic, key);
     if(!cache_total) {
         gobj_log_error(gobj, 0,
