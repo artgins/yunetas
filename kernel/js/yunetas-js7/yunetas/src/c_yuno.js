69e54317328000d3a669f8bef61480416d5a2834
Rosa <rosa.martinez@artgins.com>
2025-03-07 11:19:40 +0100

    add no_poll attr to yuno.js

--- kernel/js/yunetas-js7/yunetas/src/c_yuno.js
+++ kernel/js/yunetas-js7/yunetas/src/c_yuno.js
@@ -71,6 +71,7 @@ SDATA(data_type_t.DTP_BOOLEAN,  "trace_creation",       0,  0,      "Trace creat
 SDATA(data_type_t.DTP_BOOLEAN,  "trace_start_stop",     0,  0,      "Trace start/stop"),
 SDATA(data_type_t.DTP_BOOLEAN,  "trace_subscriptions",  0,  0,      "Trace subscription"),
 SDATA(data_type_t.DTP_BOOLEAN,  "trace_i18n",           0,  0,      "Trace i18n"),
+SDATA(data_type_t.DTP_BOOLEAN,  "no_poll",              0,  0,      "no poll"),
 SDATA(data_type_t.DTP_INTEGER,  "periodic_timeout",     0,  "1000", "Timeout periodic, in miliseconds."),
 SDATA_END()
 ];
