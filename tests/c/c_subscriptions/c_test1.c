091eae32f315351cb2878c4c6702088efe9f7e41
Rosa <rosa.martinez@artgins.com>
2025-03-17 07:18:41 +0100

    test_subs

---
+++ tests/c/c_subscriptions/c_test1.c
@@ -0,0 +1,272 @@
+/***********************************************************************
+ *          C_TEST1.C
+ *
+ *          A class to test C_TCP / C_TCP_S
+ *          Test: Use pepon as server and test the tcp client in c_test1
+ *          No interchange of messages, only connections
+ *
+ *          Tasks
+ *          - In 1 second, connecting to pepon
+ *          - In 1 seconds after connected ,dropping the connection.
+ *          - After 3 disconnections, shutdown
+ *
+ *          Copyright (c) 2024 by ArtGins.
+ *          All Rights Reserved.
+ ***********************************************************************/
+#include <iconv.h>
+#include <backtrace.h>
+
+#include "c_test1.h"
+
+/***************************************************************************
+ *              Constants
+ ***************************************************************************/
+
+/***************************************************************************
+ *              Structures
+ ***************************************************************************/
+
+/***************************************************************************
+ *              Prototypes
+ ***************************************************************************/
+
+/***************************************************************************
+ *          Data: config, public data, private data
+ ***************************************************************************/
+
+/*---------------------------------------------*
+ *      Attributes - order affect to oid's
+ *---------------------------------------------*/
+PRIVATE sdata_desc_t attrs_table[] = {
+/*-ATTR-type------------name----------------flag----------------default-----description--*/
+SDATA (DTP_INTEGER,     "timeout",          SDF_RD,             "1000",     "Timeout"),
+SDATA (DTP_POINTER,     "user_data",        0,                  0,          "user data"),
+SDATA (DTP_POINTER,     "user_data2",       0,                  0,          "more user data"),
+SDATA (DTP_POINTER,     "subscriber",       0,                  0,          "subscriber of output-events. Not a child gobj."),
+SDATA_END()
+};
+
+/*---------------------------------------------*
+ *      GClass trace levels
+ *---------------------------------------------*/
+enum {
+    TRACE_MESSAGES  = 0x0001,
+};
+PRIVATE const trace_level_t s_user_trace_level[16] = {
+{"messages",        "Trace messages"},
+{0, 0},
+};
+
+/*---------------------------------------------*
+ *              Private data
+ *---------------------------------------------*/
+typedef struct _PRIVATE_DATA {
+    json_int_t timeout;
+    hgobj timer;
+
+} PRIVATE_DATA;
+
+PRIVATE hgclass __gclass__ = 0;
+
+
+
+
+                    /******************************
+                     *      Framework Methods
+                     ******************************/
+
+
+
+
+/***************************************************************************
+ *      Framework Method create
+ ***************************************************************************/
+PRIVATE void mt_create(hgobj gobj)
+{
+    PRIVATE_DATA *priv = gobj_priv_data(gobj);
+
+    priv->timer = gobj_create_pure_child(gobj_name(gobj), C_TIMER, 0, gobj);
+
+    /*
+     *  Do copy of heavy-used parameters, for quick access.
+     *  HACK The writable attributes must be repeated in mt_writing method.
+     */
+    SET_PRIV(timeout,               gobj_read_integer_attr)
+}
+
+/***************************************************************************
+ *      Framework Method start
+ ***************************************************************************/
+PRIVATE int mt_start(hgobj gobj)
+{
+    PRIVATE_DATA *priv = gobj_priv_data(gobj);
+
+    gobj_start(priv->timer);
+
+    return 0;
+}
+
+/***************************************************************************
+ *      Framework Method stop
+ ***************************************************************************/
+PRIVATE int mt_stop(hgobj gobj)
+{
+    PRIVATE_DATA *priv = gobj_priv_data(gobj);
+
+    gobj_stop(priv->timer);
+
+    return 0;
+}
+
+/***************************************************************************
+ *      Framework Method play
+ *  Yuneta rule:
+ *  If service has mt_play then start only the service gobj.
+ *      (Let mt_play be responsible to start their tree)
+ *  If service has not mt_play then start the tree with gobj_start_tree().
+ ***************************************************************************/
+PRIVATE int mt_play(hgobj gobj)
+{
+    PRIVATE_DATA *priv = gobj_priv_data(gobj);
+
+    set_timeout(priv->timer, 1000);
+
+    return 0;
+}
+
+/***************************************************************************
+ *      Framework Method pause
+ ***************************************************************************/
+PRIVATE int mt_pause(hgobj gobj)
+{
+    PRIVATE_DATA *priv = gobj_priv_data(gobj);
+
+    clear_timeout(priv->timer);
+
+    return 0;
+}
+
+
+
+                    /***************************
+                     *      Commands
+                     ***************************/
+
+
+
+
+                    /***************************
+                     *      Local Methods
+                     ***************************/
+
+
+
+
+                    /***************************
+                     *      Actions
+                     ***************************/
+
+
+
+
+/***************************************************************************
+ *
+ ***************************************************************************/
+PRIVATE int ac_timeout(hgobj gobj, const char *event, json_t *kw, hgobj src)
+{
+
+    JSON_DECREF(kw)
+    return 0;
+}
+
+/***************************************************************************
+ *                          FSM
+ ***************************************************************************/
+/*---------------------------------------------*
+ *          Global methods table
+ *---------------------------------------------*/
+PRIVATE const GMETHODS gmt = {
+    .mt_create = mt_create,
+    .mt_start = mt_start,
+    .mt_stop = mt_stop,
+    .mt_play = mt_play,
+    .mt_pause = mt_pause,
+};
+
+/*------------------------*
+ *      GClass name
+ *------------------------*/
+GOBJ_DEFINE_GCLASS(C_TEST1);
+
+/*------------------------*
+ *      States
+ *------------------------*/
+
+/*------------------------*
+ *      Events
+ *------------------------*/
+
+/***************************************************************************
+ *
+ ***************************************************************************/
+PRIVATE int create_gclass(gclass_name_t gclass_name)
+{
+    if(__gclass__) {
+        gobj_log_error(0, 0,
+            "function",     "%s", __FUNCTION__,
+            "msgset",       "%s", MSGSET_INTERNAL_ERROR,
+            "msg",          "%s", "GClass ALREADY created",
+            "gclass",       "%s", gclass_name,
+            NULL
+        );
+        return -1;
+    }
+
+    /*----------------------------------------*
+     *          Define States
+     *----------------------------------------*/
+    ev_action_t st_idle[] = {
+        {EV_TIMEOUT,                ac_timeout,            0},
+        {0,0,0}
+    };
+    states_t states[] = {
+        {ST_IDLE,                   st_idle},
+        {0, 0}
+    };
+
+    event_type_t event_types[] = {
+        {EV_TIMEOUT,                0},
+        {0, 0}
+    };
+
+    /*----------------------------------------*
+     *          Create the gclass
+     *----------------------------------------*/
+    __gclass__ = gclass_create(
+        gclass_name,
+        event_types,
+        states,
+        &gmt,
+        0,  // lmt,
+        attrs_table,
+        sizeof(PRIVATE_DATA),
+        0,  // authz_table,
+        0,  // command_table,
+        s_user_trace_level,  // s_user_trace_level,
+        0   // gcflag_t
+    );
+    if(!__gclass__) {
+        // Error already logged
+        return -1;
+    }
+
+    return 0;
+}
+
+/***************************************************************************
+ *
+ ***************************************************************************/
+PUBLIC int register_c_test1(void)
+{
+    return create_gclass(C_TEST1);
+}
---
+++ tests/c/c_subscriptions/main_test1.c
@@ -0,0 +1,257 @@
+/****************************************************************************
+ *          MAIN.C
+ *
+ *          Test: Use pepon as server and test the tcp client in c_test1
+ *          No interchange of messages, only connections
+ *
+ *          Tasks
+ *          - In 1 second, connecting to pepon
+ *          - In 1 seconds after connected ,dropping the connection.
+ *          - After 3 disconnections, shutdown
+
+ *          Copyright (c) 2024 by ArtGins.
+ *          All Rights Reserved.
+ ****************************************************************************/
+#include <yunetas.h>
+#include "c_test1.h"
+
+/***************************************************************************
+ *                      Names
+ ***************************************************************************/
+#define APP_NAME        "test_subs_" "test1"
+#define APP_DOC         "Test Subscriptions"
+
+#define APP_VERSION     "1.0.0"
+#define APP_SUPPORT     "<support@artgins.com>"
+#define APP_DATETIME    __DATE__ " " __TIME__
+
+#define USE_OWN_SYSTEM_MEMORY   false
+#define DEBUG_MEMORY            false
+#define MEM_MIN_BLOCK           0       // use default
+#define MEM_MAX_BLOCK           0       // use default
+#define MEM_SUPERBLOCK          0       // use default
+#define MEM_MAX_SYSTEM_MEMORY   0       // use default
+
+/***************************************************************************
+ *                      Default config
+ ***************************************************************************/
+PRIVATE char fixed_config[]= "\
+{                                                                   \n\
+    'yuno': {                                                       \n\
+        'yuno_role': '"APP_NAME"',                                  \n\
+        'tags': ['test', 'yunetas']                                 \n\
+    }                                                               \n\
+}                                                                   \n\
+";
+PRIVATE char variable_config[]= "\
+{                                                                   \n\
+    'environment': {                                                \n\
+        'console_log_handlers': {                                   \n\
+        },                                                          \n\
+        'daemon_log_handlers': {                                    \n\
+        }                                                           \n\
+    },                                                              \n\
+    'yuno': {                                                       \n\
+        'autoplay': true,                                           \n\
+        'required_services': [],                                    \n\
+        'public_services': [],                                      \n\
+        'service_descriptor': {                                     \n\
+        },                                                          \n\
+        'i18n_dirname': '/yuneta/share/locale/',                    \n\
+        'i18n_domain': 'test_timer',                                \n\
+        'trace_levels': {                                           \n\
+            'C_TEST1': []       \n\
+        }                                                           \n\
+    },                                                              \n\
+    'global': {                                                     \n\
+    },                                                              \n\
+    'services': [                                                   \n\
+        {                                                           \n\
+            'name': 'c_test1',                                      \n\
+            'gclass': 'C_TEST1',                                    \n\
+            'default_service': true,                                \n\
+            'autostart': true,                                      \n\
+            'autoplay': false,                                      \n\
+            'kw': {                                                 \n\
+            },                                                      \n\
+            'zchilds': [                                            \n\
+            ]                                                       \n\
+        }                                                          \n\
+    ]                                                               \n\
+}                                                                   \n\
+";
+
+time_measure_t time_measure;
+
+/***************************************************************************
+ *  HACK This function is executed on yunetas environment (mem, log, paths)
+ *  BEFORE creating the yuno
+ ***************************************************************************/
+int result = 0;
+
+static int register_yuno_and_more(void)
+{
+    int result = 0;
+
+    /*--------------------*
+     *  Register gclass
+     *--------------------*/
+    result += register_c_test1();
+
+    /*------------------------------------------------*
+     *          Traces
+     *------------------------------------------------*/
+    // Avoid timer trace, too much information
+    gobj_set_gclass_no_trace(gclass_find_by_name(C_YUNO), "machine", true);
+    gobj_set_gclass_no_trace(gclass_find_by_name(C_TIMER0), "machine", true);
+    gobj_set_gclass_no_trace(gclass_find_by_name(C_TIMER), "machine", true);
+    gobj_set_global_no_trace("timer_periodic", true);
+
+    // Samples of traces
+    // gobj_set_gclass_trace(gclass_find_by_name(C_IEVENT_SRV), "identity-card", true);
+    // gobj_set_gclass_trace(gclass_find_by_name(C_IEVENT_CLI), "identity-card", true);
+
+    //gobj_set_gclass_trace(gclass_find_by_name(C_TEST1), "messages", true);
+    //gobj_set_gclass_trace(gclass_find_by_name(C_TEST1), "machine", true);
+
+    // gobj_set_gclass_trace(gclass_find_by_name(C_PEPON), "messages", true);
+    // gobj_set_gclass_trace(gclass_find_by_name(C_TESTON), "messages", true);
+    // gobj_set_gclass_trace(gclass_find_by_name(C_IEVENT_CLI), "ievents2", true);
+    // gobj_set_gclass_trace(gclass_find_by_name(C_IEVENT_SRV), "ievents2", true);
+    // gobj_set_gclass_trace(gclass_find_by_name(C_TCP), "traffic", true);
+
+    // Samples of global traces
+    // gobj_set_gobj_trace(0, "create_delete", true, 0);
+    // gobj_set_gobj_trace(0, "create_delete2", true, 0);
+    // gobj_set_gobj_trace(0, "start_stop", true, 0);
+    // gobj_set_gobj_trace(0, "subscriptions", true, 0);
+    // gobj_set_gobj_trace(0, "machine", true, 0);
+    // gobj_set_gobj_trace(0, "ev_kw", true, 0);
+    // gobj_set_gobj_trace(0, "liburing", true, 0);
+    // gobj_set_gobj_trace(0, "liburing_timer", true, 0);
+
+    /*------------------------------*
+     *  Start test
+     *------------------------------*/
+//    json_t *errors_list = json_pack("[{s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s}, {s:s},{s:s}, {s:s}, {s:s}, {s:s}, {s:s}]",
+//        "msg", "Starting yuno",
+//        "msg", "addrinfo on listen",
+//        "msg", "Listening...",
+//        "msg", "Playing yuno",
+//        "msg", "Connected To",
+//        "msg", "Clisrv accepted",
+//        "msg", "Connected From",
+//        "msg", "Disconnected To",
+//        "msg", "Disconnected From",
+//        "msg", "Connected To",
+//        "msg", "Clisrv accepted",
+//        "msg", "Connected From",
+//        "msg", "Disconnected To",
+//        "msg", "Disconnected From",
+//        "msg", "Connected To",
+//        "msg", "Clisrv accepted",
+//        "msg", "Connected From",
+//        "msg", "Disconnected To",
+//        "msg", "Disconnected From",
+//        "msg", "Pausing yuno",
+//        "msg", "Yuno stopped, gobj end"
+//    );
+    set_expected_results( // Check that no logs happen
+        APP_NAME, // test name
+0,//        errors_list, // errors_list,
+        NULL,   // expected, NULL: we want to check only the logs
+        NULL,   // ignore_keys
+        true    // verbose
+    );
+
+    MT_START_TIME(time_measure)
+
+    return result;
+}
+
+/***************************************************************************
+ *  HACK This function is executed on yunetas environment (mem, log, paths)
+ *  BEFORE creating the yuno
+ ***************************************************************************/
+static void cleaning(void)
+{
+    MT_INCREMENT_COUNT(time_measure, 1)
+    MT_PRINT_TIME(time_measure, APP_NAME)
+
+    result += test_json(NULL);  // NULL: we want to check only the logs
+}
+
+/***************************************************************************
+ *                      Main
+ ***************************************************************************/
+int main(int argc, char *argv[])
+{
+    /*------------------------------*
+     *  Captura salida logger
+     *------------------------------*/
+    glog_init();
+
+    /*
+     *  Add all handlers very early
+     */
+    gobj_log_add_handler("stdout", "stdout", LOG_OPT_ALL, 0);
+
+    gobj_log_register_handler(
+        "testing",          // handler_name
+        0,                  // close_fn
+        capture_log_write,  // write_fn
+        0                   // fwrite_fn
+    );
+    gobj_log_add_handler("test_capture", "testing", LOG_OPT_UP_INFO, 0);
+
+
+    /*------------------------------------------------*
+     *      To check memory loss
+     *------------------------------------------------*/
+    unsigned long memory_check_list[] = {0, 0}; // WARNING: the list ended with 0
+    set_memory_check_list(memory_check_list);
+
+    /*------------------------------------------------*
+     *      To check
+     *------------------------------------------------*/
+    // gobj_set_deep_tracing(1);
+    // set_auto_kill_time(6);
+
+    /*------------------------------------------------*
+     *          Start yuneta
+     *------------------------------------------------*/
+    helper_quote2doublequote(fixed_config);
+    helper_quote2doublequote(variable_config);
+    yuneta_setup(
+        NULL,       // persistent_attrs, default internal dbsimple
+        NULL,       // command_parser, default internal command_parser
+        NULL,       // stats_parser, default internal stats_parser
+        NULL,       // authz_checker, default Monoclass C_AUTHZ
+        NULL,       // authenticate_parser, default Monoclass C_AUTHZ
+        0,
+        0,
+        false,
+        0,
+        0
+    );
+
+    result += yuneta_entry_point(
+        argc, argv,
+        APP_NAME, APP_VERSION, APP_SUPPORT, APP_DOC, APP_DATETIME,
+        fixed_config,
+        variable_config,
+        register_yuno_and_more,
+        cleaning
+    );
+
+    if(get_cur_system_memory()!=0) {
+        printf("%sERROR --> %s%s\n", On_Red BWhite, "system memory not free", Color_Off);
+        print_track_mem();
+        result += -1;
+    }
+
+    if(result<0) {
+        printf("<-- %sTEST FAILED%s: %s\n", On_Red BWhite, Color_Off, APP_NAME);
+    }
+    return result<0?-1:0;
+}
