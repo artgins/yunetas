63cdea86da0293fbc4a9444ca9398e15d73148bc
Rosa <rosa.martinez@artgins.com>
2025-05-14 11:36:06 +0200

    stress tcp listen

--- stress/c/listen/main_listen.c
+++ stress/c/listen/main_listen.c
@@ -15,8 +15,8 @@
 /***************************************************************************
  *                      Names
  ***************************************************************************/
-#define APP_NAME        "test_listen"
-#define APP_DOC         "Test Listen"
+#define APP_NAME        "stress_listen"
+#define APP_DOC         "Stress TCP Listen"
 
 #define APP_VERSION     "1.0.0"
 #define APP_SUPPORT     "<support@artgins.com>"
@@ -104,7 +104,7 @@ PRIVATE char variable_config[]= "\
                 }                                                   \n\
             ],                                                      \n\
             '[^^children^^]': {                                     \n\
-                '__range__': [[1,1]],                               \n\
+                '__range__': [[1,10]],                              \n\
                 '__vars__': {                                       \n\
                 },                                                  \n\
                 '__content__': {                                    \n\
