afdd5b384ecd17fe8280b9ddcbaf095766a90a94
Rosa <rosa.martinez@artgins.com>
2025-05-23 15:19:33 +0200

    print date

--- stress/c/listen/listen.sh
+++ stress/c/listen/listen.sh
@@ -1,1 +1,3 @@
+date
+
 ./stress_listen --config-file="[\"/yuneta/realms/artgins/utilities.artgins.com/stress_listen^10/bin/1-stress_listen^10.json\",\"/yuneta/realms/artgins/utilities.artgins.com/stress_listen^10/bin/2-stress_listen^10.json\",\"/yuneta/realms/artgins/utilities.artgins.com/stress_listen^10/bin/3-stress_listen^10.json\"]" $1
