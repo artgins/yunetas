9ebd86a67d12500fc01183ea40d7b90563d0f423
Rosa <rosa.martinez@artgins.com>
2025-05-24 08:51:44 +0200

    number of channels

--- kernel/c/root-linux/src/c_iogate.c
+++ kernel/c/root-linux/src/c_iogate.c
@@ -390,7 +390,7 @@ PRIVATE json_t *cmd_view_channels(hgobj gobj, const char *cmd, json_t *kw, hgobj
     return msg_iev_build_response(
         gobj,
         0,
-        0,
+        json_sprintf("%d channels", (int)json_array_size(jn_data)),
         0,
         jn_data, // owned
         kw  // owned
