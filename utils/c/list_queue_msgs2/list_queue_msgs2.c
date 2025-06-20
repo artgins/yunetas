2bbf4ffd92a7a1df77d15e65856f6556d534b3fe
Rosa <rosa.martinez@artgins.com>
2025-06-20 17:50:38 +0200

    compile all

--- utils/c/list_queue_msgs2/list_queue_msgs2.c
+++ utils/c/list_queue_msgs2/list_queue_msgs2.c
@@ -7,7 +7,7 @@
  *          All Rights Reserved.
  ****************************************************************************/
 #include <stdio.h>
-#include <argp.h>
+#include <argp-standalone.h>
 #include <time.h>
 #include <errno.h>
 #include <stdint.h>
