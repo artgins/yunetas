628758d5ea551bb32425e98e5d0e9a4a89a0c901
Rosa <rosa.martinez@artgins.com>
2025-06-25 10:42:57 +0200

    fix include

--- kernel/c/libjwt/src/mbedtls/jwk-parse.c
+++ kernel/c/libjwt/src/mbedtls/jwk-parse.c
@@ -6,6 +6,7 @@
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <string.h>
 #include "../jwt.h"
 
 #include "../jwt-private.h"
