d2d759f74679e339cd6e1fbecaafa0295bbd9dbd
Rosa <rosa.martinez@artgins.com>
2025-07-27 17:56:15 +0200

    fix {EV_MT_COMMAND_ANSWER,      EVF_PUBLIC_EVENT},

--- kernel/c/root-linux/src/c_ievent_cli.c
+++ kernel/c/root-linux/src/c_ievent_cli.c
@@ -1541,7 +1541,7 @@ PRIVATE int create_gclass(gclass_name_t gclass_name)
 
     event_type_t event_types[] = {
         {EV_ON_MESSAGE,             0},
-        {EV_MT_COMMAND_ANSWER,      0},
+        {EV_MT_COMMAND_ANSWER,      EVF_PUBLIC_EVENT},
         {EV_MT_COMMAND,             EVF_PUBLIC_EVENT},
         {EV_MT_STATS,               EVF_PUBLIC_EVENT},
         {EV_IDENTITY_CARD_ACK,      EVF_PUBLIC_EVENT},
