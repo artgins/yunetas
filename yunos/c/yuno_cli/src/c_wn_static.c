414c1e8a41fb54d009632b3c89e5e747945b6f32
Rosa <rosa.martinez@artgins.com>
2025-08-09 10:40:02 +0200

    wn_static

--- yunos/c/yuno_cli/src/c_wn_static.c
+++ yunos/c/yuno_cli/src/c_wn_static.c
@@ -2,9 +2,37 @@
  *          C_WN_STATIC.C
  *          Wn_static GClass.
  *
- *          Static control
+ *          - Manages LOGICAL size, text, position, size and color.
+ *          - It creates a new WINDOW and PANE (sorry?)
+ *
+ *          It has the mt_play/mt_pause,  to disable or not the box,
+ *              - changing the state ST_DISABLED,ST_IDLE
+ *
+ *          It has the mt_child_added, mt_child_removed:
+ *              - mt_child_added:
+ *                  - fix_child_sizes()
+ *                  - send EV_PAINT to children
+ *              - mt_child_removed:
+ *                  - fix_child_sizes()
+ *                  - send EV_PAINT to children
+ *
+ *          It supports the events:
+ *              EV_SETTEXT
+ *                  - write "text" attribute
+ *                  - auto-send EV_PAINT
+ *              EV_PAINT
+ *                  - clear the WINDOW
+ *                  - it it has color set the color in WINDOW (back/fore)???
+ *                  - write "text" value in WINDOW!
+ *                  - update PANE or WINDOW
+ *              EV_MOVE
+ *                  - update PANE or WINDOW
+ *              EV_SIZE
+ *                  - update PANE or WINDOW
+ *                  - auto-send EV_PAINT
  *
  *          Copyright (c) 2016 Niyamaka.
+ *          Copyright (c) 2025, ArtGins.
  *          All Rights Reserved.
 ***********************************************************************/
 #include <string.h>
