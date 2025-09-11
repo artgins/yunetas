8e74f921ece8ea93dea489769a3e57f01bc3b51b
Rosa <rosa.martinez@artgins.com>
2025-09-11 14:00:11 +0200

    clean includes

--- utils/c/yclone-project/make_clone.c
+++ utils/c/yclone-project/make_clone.c
@@ -7,7 +7,6 @@
  ****************************************************************************/
 #include <sys/types.h>
 #include <dirent.h>
-#include <time.h>
 #include <sys/stat.h>
 #include <libgen.h>
 #include <stdlib.h>
