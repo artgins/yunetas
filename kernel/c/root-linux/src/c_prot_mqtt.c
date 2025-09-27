0989b305c36b2c7aa32164af65463cd127619fae
Rosa <rosa.martinez@artgins.com>
2025-09-27 10:08:23 +0200

    reviewing mqtt: error codes don't match

--- kernel/c/root-linux/src/c_prot_mqtt.c
+++ kernel/c/root-linux/src/c_prot_mqtt.c
@@ -338,42 +338,42 @@ typedef enum mosquitto_protocol {
 
 /* Error values */
 typedef enum mosq_err_t {
+    MOSQ_ERR_AUTH_CONTINUE = -4,
+    MOSQ_ERR_NO_SUBSCRIBERS = -3,
+    MOSQ_ERR_SUB_EXISTS = -2,
+    MOSQ_ERR_CONN_PENDING = -1,
     MOSQ_ERR_SUCCESS = 0,
-    MOSQ_ERR_PROTOCOL = -2,
-    MOSQ_ERR_INVAL = -3,
-    MOSQ_ERR_NO_CONN = -4,
-    MOSQ_ERR_CONN_REFUSED = -5,
-    MOSQ_ERR_NOT_FOUND = -6,
-    MOSQ_ERR_CONN_LOST = -7,
-    MOSQ_ERR_TLS = -8,
-    MOSQ_ERR_PAYLOAD_SIZE = -9,
-    MOSQ_ERR_NOT_SUPPORTED = -10,
-    MOSQ_ERR_AUTH = -11,
-    MOSQ_ERR_ACL_DENIED = -12,
-    MOSQ_ERR_UNKNOWN = -13,
-    MOSQ_ERR_ERRNO = -14,
-    MOSQ_ERR_EAI = -15,
-    MOSQ_ERR_PROXY = -16,
-    MOSQ_ERR_PLUGIN_DEFER = -17,
-    MOSQ_ERR_MALFORMED_UTF8 = -18,
-    MOSQ_ERR_KEEPALIVE = -19,
-    MOSQ_ERR_LOOKUP = -20,
-    MOSQ_ERR_MALFORMED_PACKET = -21,
-    MOSQ_ERR_DUPLICATE_PROPERTY = -22,
-    MOSQ_ERR_TLS_HANDSHAKE = -23,
-    MOSQ_ERR_QOS_NOT_SUPPORTED = -24,
-    MOSQ_ERR_OVERSIZE_PACKET = -25,
-    MOSQ_ERR_OCSP = -26,
-    MOSQ_ERR_TIMEOUT = -27,
-    MOSQ_ERR_RETAIN_NOT_SUPPORTED = -28,
-    MOSQ_ERR_TOPIC_ALIAS_INVALID = -29,
-    MOSQ_ERR_ADMINISTRATIVE_ACTION = -30,
-    MOSQ_ERR_ALREADY_EXISTS = -31,
-    MOSQ_ERR_NOMEM = -32,
-    MOSQ_ERR_AUTH_CONTINUE = -44,
-    MOSQ_ERR_NO_SUBSCRIBERS = -43,
-    MOSQ_ERR_SUB_EXISTS = -42,
-    MOSQ_ERR_CONN_PENDING = -41,
+    MOSQ_ERR_NOMEM = 1,
+    MOSQ_ERR_PROTOCOL = 2,
+    MOSQ_ERR_INVAL = 3,
+    MOSQ_ERR_NO_CONN = 4,
+    MOSQ_ERR_CONN_REFUSED = 5,
+    MOSQ_ERR_NOT_FOUND = 6,
+    MOSQ_ERR_CONN_LOST = 7,
+    MOSQ_ERR_TLS = 8,
+    MOSQ_ERR_PAYLOAD_SIZE = 9,
+    MOSQ_ERR_NOT_SUPPORTED = 10,
+    MOSQ_ERR_AUTH = 11,
+    MOSQ_ERR_ACL_DENIED = 12,
+    MOSQ_ERR_UNKNOWN = 13,
+    MOSQ_ERR_ERRNO = 14,
+    MOSQ_ERR_EAI = 15,
+    MOSQ_ERR_PROXY = 16,
+    MOSQ_ERR_PLUGIN_DEFER = 17,
+    MOSQ_ERR_MALFORMED_UTF8 = 18,
+    MOSQ_ERR_KEEPALIVE = 19,
+    MOSQ_ERR_LOOKUP = 20,
+    MOSQ_ERR_MALFORMED_PACKET = 21,
+    MOSQ_ERR_DUPLICATE_PROPERTY = 22,
+    MOSQ_ERR_TLS_HANDSHAKE = 23,
+    MOSQ_ERR_QOS_NOT_SUPPORTED = 24,
+    MOSQ_ERR_OVERSIZE_PACKET = 25,
+    MOSQ_ERR_OCSP = 26,
+    MOSQ_ERR_TIMEOUT = 27,
+    MOSQ_ERR_RETAIN_NOT_SUPPORTED = 28,
+    MOSQ_ERR_TOPIC_ALIAS_INVALID = 29,
+    MOSQ_ERR_ADMINISTRATIVE_ACTION = 30,
+    MOSQ_ERR_ALREADY_EXISTS = 31,
 } mosq_err_t;
 
 /* Option values */
@@ -4026,7 +4026,7 @@ PRIVATE int handle__pingreq(hgobj gobj)
             "msg",          "%s", "Mqtt CMD_PINGREQ: not in session",
             NULL
         );
-        return -1;
+        return MOSQ_ERR_PROTOCOL;
     }
     if(!priv->iamServer) {
         gobj_log_error(gobj, 0,
@@ -4035,7 +4035,7 @@ PRIVATE int handle__pingreq(hgobj gobj)
             "msg",          "%s", "Mqtt CMD_PINGREQ: not server",
             NULL
         );
-        return -1;
+        return MOSQ_ERR_PROTOCOL;
     }
 
     if(priv->frame_head.flags != 0) {
@@ -4061,17 +4061,24 @@ PRIVATE int handle__pingresp(hgobj gobj)
             "msg",          "%s", "Mqtt CMD_PINGRESP: not in session",
             NULL
         );
-        return -1;
-    }
-
-    // mosq->ping_t = 0; /* No longer waiting for a PINGRESP. */
-
-    if(!priv->is_bridge) {
-        // Parece que el broker no debe recibir pingresp
         return MOSQ_ERR_PROTOCOL;
     }
 
-    return send_simple_command(gobj, CMD_PINGRESP);
+    // priv->ping_t = 0; TODO /* No longer waiting for a PINGRESP. */
+
+    if(priv->iamServer) {
+        if(!priv->is_bridge) {
+            // Parece que el broker no debe recibir pingresp, solo si es bridge
+            gobj_log_error(gobj, 0,
+                "function",     "%s", __FUNCTION__,
+                "msgset",       "%s", MSGSET_MQTT_ERROR,
+                "msg",          "%s", "Mqtt CMD_PINGREQ: not server",
+                NULL
+            );
+            return MOSQ_ERR_PROTOCOL;
+        }
+    }
+    return MOSQ_ERR_SUCCESS;
 }
 
 /***************************************************************************
