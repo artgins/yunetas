3b4b27d17856b590c633d70a159c588ee22dac2d
Rosa <rosa.martinez@artgins.com>
2025-09-27 20:33:10 +0200

    add /yuneta/development/tools/cmake to deb

--- packages/make-yuneta-agent-deb.sh
+++ packages/make-yuneta-agent-deb.sh
@@ -128,7 +128,7 @@ copy_tree "/yuneta/bin/nginx"                   "${WORKDIR}/yuneta/bin"
 copy_tree "/yuneta/bin/openresty"               "${WORKDIR}/yuneta/bin"
 copy_tree "/yuneta/development/outputs_ext"     "${WORKDIR}/yuneta/development"
 copy_tree "/yuneta/development/outputs"         "${WORKDIR}/yuneta/development"
-copy_tree "/yuneta/development/tools"           "${WORKDIR}/yuneta/development"
+copy_tree "/yuneta/development/yunetas/tools"   "${WORKDIR}/yuneta/development"
 copy_tree "/yuneta/bin/skeletons"               "${WORKDIR}/yuneta/bin"
 
 # --- Optional: bundle SSH public key(s) for user 'yuneta' ---
