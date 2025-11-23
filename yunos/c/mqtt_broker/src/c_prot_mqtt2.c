285634a34abc470d483f17026d1bce659f5ea66e
Rosa <rosa.martinez@artgins.com>
2025-11-23 09:36:10 +0100

    mqtt client

--- yunos/c/mqtt_broker/src/c_prot_mqtt2.c
+++ yunos/c/mqtt_broker/src/c_prot_mqtt2.c
@@ -2841,6 +2841,186 @@ PRIVATE int send_simple_command(hgobj gobj, uint8_t command)
 /***************************************************************************
  *
  ***************************************************************************/
+PRIVATE int send__connect(
+    hgobj gobj,
+    uint16_t keepalive,
+    BOOL clean_session,
+    json_t *properties
+) {
+    uint32_t payloadlen;
+    uint8_t will = 0;
+    uint8_t byte;
+    int rc;
+    uint8_t version;
+    uint32_t headerlen;
+    uint32_t proplen = 0, varbytes = 0;
+    json_t *local_props = NULL;
+    uint16_t receive_maximum;
+
+    PRIVATE_DATA *priv = gobj_priv_data(gobj);
+
+    if(gobj_trace_level(gobj) & SHOW_DECODE) {
+        trace_msg0("👉👉 Sending CONNECT to '%s' %s",
+            priv->client_id,
+            gobj_short_name(gobj_bottom_gobj(gobj))
+        );
+        if(properties) {
+            gobj_trace_json(gobj, properties, "Sending CONNECT properties");
+        }
+    }
+
+    int protocol = (int)gobj_read_integer_attr(gobj, "mqtt_protocol");
+    const char *clientid = gobj_read_str_attr(gobj, "mqtt_client_id");
+    if(protocol == mosq_p_mqtt31 && empty_string(clientid)) {
+        // TODO log error and check error return
+        return MOSQ_ERR_PROTOCOL;
+    }
+
+    const char *username = gobj_read_str_attr(gobj, "user_id");
+    const char *password = gobj_read_str_attr(gobj, "user_passw");
+
+    /*
+     *  Work with nulls
+     */
+    if(empty_string(clientid)) {
+        clientid = NULL;
+    }
+    if(empty_string(username)) {
+        username = NULL;
+    }
+    if(empty_string(password)) {
+        password = NULL;
+    }
+
+    if(protocol == mosq_p_mqtt5) {
+        /* Generate properties from options */
+        // if(!mosquitto_property_read_int16(properties, MQTT_PROP_RECEIVE_MAXIMUM, &receive_maximum, false)) {
+        //     rc = mosquitto_property_add_int16(&local_props, MQTT_PROP_RECEIVE_MAXIMUM, mosq->msgs_in.inflight_maximum);
+        //     if(rc) return rc;
+        // }else{
+        //     mosq->msgs_in.inflight_maximum = receive_maximum;
+        //     mosq->msgs_in.inflight_quota = receive_maximum;
+        // }
+
+        version = MQTT_PROTOCOL_V5;
+        headerlen = 10;
+        proplen = 0;
+        // proplen += property__get_length_all(properties);
+        // proplen += property__get_length_all(local_props);
+        // varbytes = packet__varint_bytes(proplen);
+        headerlen += proplen + varbytes;
+    } else if(protocol == mosq_p_mqtt311){
+        version = MQTT_PROTOCOL_V311;
+        headerlen = 10;
+    } else if(protocol == mosq_p_mqtt31){
+        version = MQTT_PROTOCOL_V31;
+        headerlen = 12;
+    } else {
+        return MOSQ_ERR_INVAL;
+    }
+
+    if(clientid) {
+        payloadlen = (uint32_t)(2U+strlen(clientid));
+    } else {
+        payloadlen = 2U;
+    }
+
+    // if(mosq->will) { TODO
+    //     will = 1;
+    //     assert(mosq->will->msg.topic);
+    //
+    //     payloadlen += (uint32_t)(2+strlen(mosq->will->msg.topic) + 2+(uint32_t)mosq->will->msg.payloadlen);
+    //     if(protocol == mosq_p_mqtt5){
+    //         payloadlen += property__get_remaining_length(mosq->will->properties);
+    //     }
+    // }
+
+    /* After this check we can be sure that the username and password are
+     * always valid for the current protocol, so there is no need to check
+     * username before checking password. */
+    if(protocol == mosq_p_mqtt31 || protocol == mosq_p_mqtt311) {
+        if(password != NULL && username == NULL) {
+            return MOSQ_ERR_INVAL;
+        }
+    }
+
+    if(username) {
+        payloadlen += (uint32_t)(2+strlen(username));
+    }
+    if(password) {
+        payloadlen += (uint32_t)(2+strlen(password));
+    }
+
+    int remaining_length = headerlen + payloadlen;
+    gbuffer_t *gbuf = build_mqtt_packet(gobj, CMD_CONNECT, remaining_length);
+    if(!gbuf) {
+        // Error already logged
+        JSON_DECREF(properties);
+        return MOSQ_ERR_NOMEM;
+    }
+
+    /* Variable header */
+    if(version == MQTT_PROTOCOL_V31) {
+        mqtt_write_string(gbuf, PROTOCOL_NAME_v31);
+    }else{
+        mqtt_write_string(gbuf, PROTOCOL_NAME);
+    }
+    mqtt_write_byte(gbuf, version);
+    byte = (uint8_t)((clean_session&0x1)<<1);
+    if(will) {
+        // TODO
+        // byte = byte | (uint8_t)(((mosq->will->msg.qos&0x3)<<3) | ((will&0x1)<<2));
+        // if(mosq->retain_available) {
+        //     byte |= (uint8_t)((mosq->will->msg.retain&0x1)<<5);
+        // }
+    }
+    if(username) {
+        byte = byte | 0x1<<7;
+    }
+    if(password) {
+        byte = byte | 0x1<<6;
+    }
+    mqtt_write_byte(gbuf, byte);
+    mqtt_write_uint16(gbuf, keepalive);
+
+    if(protocol == mosq_p_mqtt5) {
+        /* Write properties */
+        mqtt_write_varint(gbuf, proplen);
+        property_write_all(gobj, gbuf, properties, FALSE);
+        property_write_all(gobj, gbuf, local_props, FALSE);
+    }
+    JSON_DECREF(local_props)
+
+    /* Payload */
+    if(clientid) {
+        mqtt_write_string(gbuf, clientid);
+    } else {
+        mqtt_write_uint16(gbuf, 0);
+    }
+    if(will) {
+        // TODO
+        // if(protocol == mosq_p_mqtt5) {
+        //     /* Write will properties */
+        //     property_write_all(gobj, gbuf, mosq->will->properties, true);
+        // }
+        // mqtt_write_string(gbuf, mosq->will->msg.topic, (uint16_t)strlen(mosq->will->msg.topic));
+        // mqtt_write_string(gbuf, (const char *)mosq->will->msg.payload, (uint16_t)mosq->will->msg.payloadlen);
+    }
+
+    if(username) {
+        mqtt_write_string(gbuf, username);
+    }
+    if(password) {
+        mqtt_write_string(gbuf, password);
+    }
+
+    // mosq->keepalive = keepalive;
+    return send_packet(gobj, gbuf);
+}
+
+/***************************************************************************
+ *
+ ***************************************************************************/
 PRIVATE int send__connack(
     hgobj gobj,
     uint8_t ack,
@@ -5414,168 +5594,6 @@ PRIVATE int frame_completed(hgobj gobj)
 
     start_wait_frame_header(gobj);
     return ret;
-}
-
-/***************************************************************************
- *
- ***************************************************************************/
-PRIVATE int send__connect(
-    hgobj gobj,
-    uint16_t keepalive,
-    BOOL clean_session,
-    json_t *properties
-) {
-    uint32_t payloadlen;
-    uint8_t will = 0;
-    uint8_t byte;
-    int rc;
-    uint8_t version;
-    uint32_t headerlen;
-    uint32_t proplen = 0, varbytes = 0;
-    json_t *local_props = NULL;
-    uint16_t receive_maximum;
-
-    PRIVATE_DATA *priv = gobj_priv_data(gobj);
-
-    int protocol = (int)gobj_read_integer_attr(gobj, "mqtt_protocol");
-    const char *clientid = gobj_read_str_attr(gobj, "mqtt_client_id");
-    if(protocol == mosq_p_mqtt31 && empty_string(clientid)) {
-        // TODO log error and check error return
-        return MOSQ_ERR_PROTOCOL;
-    }
-
-    const char *username = gobj_read_str_attr(gobj, "user_id");
-    const char *password = gobj_read_str_attr(gobj, "user_passw");
-    if(empty_string(username)) {
-        username = NULL;
-    }
-    if(empty_string(password)) {
-        password = NULL;
-    }
-
-    if(protocol == mosq_p_mqtt5) {
-        /* Generate properties from options */
-        // if(!mosquitto_property_read_int16(properties, MQTT_PROP_RECEIVE_MAXIMUM, &receive_maximum, false)) {
-        //     rc = mosquitto_property_add_int16(&local_props, MQTT_PROP_RECEIVE_MAXIMUM, mosq->msgs_in.inflight_maximum);
-        //     if(rc) return rc;
-        // }else{
-        //     mosq->msgs_in.inflight_maximum = receive_maximum;
-        //     mosq->msgs_in.inflight_quota = receive_maximum;
-        // }
-
-        version = MQTT_PROTOCOL_V5;
-        headerlen = 10;
-        proplen = 0;
-        // proplen += property__get_length_all(properties);
-        // proplen += property__get_length_all(local_props);
-        // varbytes = packet__varint_bytes(proplen);
-        headerlen += proplen + varbytes;
-    } else if(protocol == mosq_p_mqtt311){
-        version = MQTT_PROTOCOL_V311;
-        headerlen = 10;
-    } else if(protocol == mosq_p_mqtt31){
-        version = MQTT_PROTOCOL_V31;
-        headerlen = 12;
-    } else {
-        return MOSQ_ERR_INVAL;
-    }
-
-    if(clientid) {
-        payloadlen = (uint32_t)(2U+strlen(clientid));
-    } else {
-        payloadlen = 2U;
-    }
-
-    // if(mosq->will){
-    //     will = 1;
-    //     assert(mosq->will->msg.topic);
-    //
-    //     payloadlen += (uint32_t)(2+strlen(mosq->will->msg.topic) + 2+(uint32_t)mosq->will->msg.payloadlen);
-    //     if(protocol == mosq_p_mqtt5){
-    //         payloadlen += property__get_remaining_length(mosq->will->properties);
-    //     }
-    // }
-
-    /* After this check we can be sure that the username and password are
-     * always valid for the current protocol, so there is no need to check
-     * username before checking password. */
-    if(protocol == mosq_p_mqtt31 || protocol == mosq_p_mqtt311) {
-        if(password != NULL && username == NULL) {
-            return MOSQ_ERR_INVAL;
-        }
-    }
-
-    if(username) {
-        payloadlen += (uint32_t)(2+strlen(username));
-    }
-    if(password) {
-        payloadlen += (uint32_t)(2+strlen(password));
-    }
-
-    packet->command = CMD_CONNECT;
-    packet->remaining_length = headerlen + payloadlen;
-    rc = packet__alloc(packet);
-    if(rc) {
-        mosquitto__free(packet);
-        return rc;
-    }
-
-    /* Variable header */
-    if(version == MQTT_PROTOCOL_V31) {
-        packet__write_string(packet, PROTOCOL_NAME_v31, (uint16_t)strlen(PROTOCOL_NAME_v31));
-    }else{
-        packet__write_string(packet, PROTOCOL_NAME, (uint16_t)strlen(PROTOCOL_NAME));
-    }
-    packet__write_byte(packet, version);
-    byte = (uint8_t)((clean_session&0x1)<<1);
-    if(will) {
-        byte = byte | (uint8_t)(((mosq->will->msg.qos&0x3)<<3) | ((will&0x1)<<2));
-        if(mosq->retain_available) {
-            byte |= (uint8_t)((mosq->will->msg.retain&0x1)<<5);
-        }
-    }
-    if(username) {
-        byte = byte | 0x1<<7;
-    }
-    if(password) {
-        byte = byte | 0x1<<6;
-    }
-    packet__write_byte(packet, byte);
-    packet__write_uint16(packet, keepalive);
-
-    if(protocol == mosq_p_mqtt5){
-        /* Write properties */
-        packet__write_varint(packet, proplen);
-        property__write_all(packet, properties, false);
-        property__write_all(packet, local_props, false);
-    }
-    mosquitto_property_free_all(&local_props);
-
-    /* Payload */
-    if(clientid) {
-        packet__write_string(packet, clientid, (uint16_t)strlen(clientid));
-    } else {
-        packet__write_uint16(packet, 0);
-    }
-    if(will) {
-        if(protocol == mosq_p_mqtt5) {
-            /* Write will properties */
-            property__write_all(packet, mosq->will->properties, true);
-        }
-        packet__write_string(packet, mosq->will->msg.topic, (uint16_t)strlen(mosq->will->msg.topic));
-        packet__write_string(packet, (const char *)mosq->will->msg.payload, (uint16_t)mosq->will->msg.payloadlen);
-    }
-
-    if(username) {
-        packet__write_string(packet, username, (uint16_t)strlen(username));
-    }
-    if(password) {
-        packet__write_string(packet, password, (uint16_t)strlen(password));
-    }
-
-    mosq->keepalive = keepalive;
-    log__printf(mosq, MOSQ_LOG_DEBUG, "Client %s sending CONNECT", SAFE_PRINT(clientid));
-    return packet__queue(mosq, packet);
 }
 
 
