fe46d541c46319f0f5a2d399b51f5fde4d92a491
Core <core@artgins.com>
2026-09-17 19:43:40 +0200

    gui_agent and gui_treedb added as roles permitted to agent

--- yunos/c/yuno_agent/src/c_agent.c
+++ yunos/c/yuno_agent/src/c_agent.c
@@ -11137,13 +11137,15 @@ PRIVATE int ac_on_open(hgobj gobj, gobj_event_t event, json_t *kw, hgobj src)
 
     const char *client_yuno_role = kw_get_str(gobj, kw, "client_yuno_role", "", 0);
     if(strcasecmp(client_yuno_role, "yuneta_cli")==0 ||
-            strcasecmp(client_yuno_role, "yuneta_agent")==0 ||
-            strcasecmp(client_yuno_role, "ycli")==0 ||
-            strcasecmp(client_yuno_role, "ybatch")==0 ||
-            strcasecmp(client_yuno_role, "ystats")==0 ||
-            strcasecmp(client_yuno_role, "ycommand")==0 ||
-            strcasecmp(client_yuno_role, "ytests")==0 ||
-            strcasecmp(client_yuno_role, "yuneta_gui")==0) {
+        strcasecmp(client_yuno_role, "yuneta_agent")==0 ||
+        strcasecmp(client_yuno_role, "ycli")==0 ||
+        strcasecmp(client_yuno_role, "ybatch")==0 ||
+        strcasecmp(client_yuno_role, "ystats")==0 ||
+        strcasecmp(client_yuno_role, "ycommand")==0 ||
+        strcasecmp(client_yuno_role, "ytests")==0 ||
+        strcasecmp(client_yuno_role, "gui_agent")==0 ||
+        strcasecmp(client_yuno_role, "gui_treedb")==0
+    ) {
         // let it.
 
         KW_DECREF(kw);
